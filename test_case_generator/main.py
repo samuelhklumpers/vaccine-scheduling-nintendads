a55410d299f55895e0d81bc3e31ff64c81778de0
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2021-09-24 19:37:21 +0200

    added some more test cases

--- test_case_generator/main.py
+++ test_case_generator/main.py
@@ -2,16 +2,27 @@ from random import randrange
 
 
 class Problem:
-    def __init__(self, processing_time_first_dose, processing_time_second_dose, gap_between_doses) -> None:
+    def __init__(self, processing_time_first_dose, processing_time_second_dose, gap_between_doses):
+        # assert that all input is valid
+        assert processing_time_first_dose >= 0
+        assert processing_time_second_dose >= 0
+        assert gap_between_doses >= 0
+
         self.processing_time_first_dose = processing_time_first_dose
         self.processing_time_second_dose = processing_time_second_dose
         self.gap_between_doses = gap_between_doses
         self.patient_list = []
 
     def add_patient(self, patient):
+        # assert that the given patient details are valid within this problem
+        assert patient.last_timeslot_first_dose - \
+            patient.first_timeslot_first_dose + 1 >= self.processing_time_first_dose
+        assert patient.length_second_dose_interval >= self.processing_time_second_dose
+
         self.patient_list.append(patient)
 
     def write_to_file_as_offline_problem(self, filename):
+        # write the problem as an offline problem to a given file
         with open(filename, "w") as file1:
             file1.write(str(self.processing_time_first_dose) + "\n")
             file1.write(str(self.processing_time_second_dose) + "\n")
@@ -21,6 +32,7 @@ class Problem:
                 file1.write("\n" + str(patient))
 
     def write_to_file_as_online_problem(self, filename):
+        # write the problem as an online problem to a given file
         with open(filename, "w") as file1:
             file1.write(str(self.processing_time_first_dose) + "\n")
             file1.write(str(self.processing_time_second_dose) + "\n")
@@ -31,6 +43,12 @@ class Problem:
 
 class Patient:
     def __init__(self, first_timeslot_first_dose, last_timeslot_first_dose, delay_between_doses, length_second_dose_interval):
+        # assert that all patient details are valid
+        assert first_timeslot_first_dose >= 0
+        assert last_timeslot_first_dose >= 0
+        assert length_second_dose_interval >= 0
+        assert delay_between_doses >= 0
+
         self.first_timeslot_first_dose = first_timeslot_first_dose
         self.last_timeslot_first_dose = last_timeslot_first_dose
         self.delay_between_doses = delay_between_doses
@@ -42,39 +60,43 @@ class Patient:
 
 
 def completely_random_n_patients(n):
-    p = Problem(randrange(0, 20), randrange(0, 20), randrange(0, 20))
+    p = Problem(randrange(1, 20), randrange(1, 20), randrange(1, 20))
     for _ in range(n):
-        # check if processing time fits in intervals
-        first_timeslot = randrange(0, 20)
-        p.add_patient(Patient(first_timeslot, first_timeslot +
-                              randrange(1, 20), randrange(0, 20), randrange(0, 20)))
-    p.write_to_file_as_offline_problem("random_offline.txt")
+        # generate patient data that would fit in the problem, paying special attention to intervals and processing times
+        first_timeslot_first_dose = randrange(0, 20)
+        last_timeslot_first_dose = randrange(
+            first_timeslot_first_dose + p.processing_time_first_dose, first_timeslot_first_dose + p.processing_time_first_dose + 20)
+        delay_between_doses = randrange(1, 20)
+        length_second_dose_interval = randrange(
+            p.processing_time_second_dose, p.processing_time_second_dose + 20)
+
+        p.add_patient(Patient(first_timeslot_first_dose, last_timeslot_first_dose,
+                              delay_between_doses, length_second_dose_interval))
+    p.write_to_file_as_offline_problem(
+        "../data/offline/random_" + str(n) + ".txt")
+    p.write_to_file_as_online_problem(
+        "../data/online/random_" + str(n) + ".txt")
 
 
-def example_test_case_offline():
+def example_test_case():
     p = Problem(2, 3, 1)
     p.add_patient(Patient(2, 7, 5, 4))
     p.add_patient(Patient(1, 2, 3, 3))
     p.add_patient(Patient(2, 4, 0, 5))
-    p.write_to_file_as_offline_problem("assignment_offline.txt")
-
-
-def example_test_case_online():
-    p = Problem(2, 3, 1)
-    p.add_patient(Patient(2, 7, 5, 4))
-    p.add_patient(Patient(1, 2, 3, 3))
-    p.add_patient(Patient(2, 4, 0, 5))
-    p.write_to_file_as_online_problem("assignment_online.txt")
+    p.write_to_file_as_offline_problem("../data/offline/from_assignment.txt")
+    p.write_to_file_as_online_problem("../data/online/from_assignment.txt")
 
 
 def big_numbers():
-    p = Problem(100000, 100000, 100000)
-    p.add_patient(Patient(100000, 200000, 100000, 100000))
-    p.add_patient(Patient(200001, 300000, 100000, 100000))
-    p.write_to_file_as_offline_problem("big_numbers.txt")
+    p = Problem(100_000_000, 100_000_000, 100_000_000)
+    p.add_patient(Patient(0, 99_999_999, 100_000_000, 100_000_000))
+    p.add_patient(Patient(100_000_000, 199_999_999, 100_000_000, 100_000_000))
+    p.write_to_file_as_offline_problem("../data/offline/big_numbers.txt")
+    p.write_to_file_as_online_problem("../data/online/big_numbers.txt")
 
 
-example_test_case_offline()
-example_test_case_online()
+example_test_case()
+completely_random_n_patients(5)
 completely_random_n_patients(20)
+completely_random_n_patients(100)
 big_numbers()
