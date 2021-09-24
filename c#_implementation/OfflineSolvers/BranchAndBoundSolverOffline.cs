04658b9036e51e1045f723fea36206212de26b16
arjamentink <a.mentink@students.uu.nl>
2021-09-24 10:29:48 +0200

    new branch

---
+++ c#_implementation/OfflineSolvers/BranchAndBoundSolverOffline.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace implementation
+{
+    class BranchAndBoundSolverOffline : ISolverOffline
+    {
+
+
+
+        
+
+
+
+    }
+}
